f6f1fb45906056b147e93ac37e6bcce244df2c00
geasseur <baptiste.ventura@neuf.fr>
2017-08-22 10:58:18 +0200

    first commit, exo 1 2 and 3 done

---
+++ js/exo.js
@@ -0,0 +1,263 @@
+/*function user(prenom,nom,paysResidence,age,mPayement){
+  this.prenom = prenom;
+  this.nom = nom;
+  this.paysResidence = paysResidence;
+  this.age = age;
+  this.mPayement = mPayement.split(",");
+  this.alertPrenom = function (prenom) {
+        alert(this.prenom);
+    };
+  this.alertNom = function(nom){
+        alert(this.nom);
+  };
+  this.anneeNaissance = function(mPayement){
+    var date = new Date();
+    console.log(date);
+    var annee = date.getFullYear();
+    var anneeNaissance = annee - age;
+    alert(anneeNaissance);
+  };
+}
+
+var baptiste = new user("baptiste","ventura","france","25","mastercard, visa");
+/*
+baptiste.alertPrenom();
+baptiste.alertNom();
+baptiste.anneeNaissance();*/
+
+/*
+var verificationUser ={
+  paysAutorise : ["france","allemagne","Suede"],
+  payementAutorise:["mastercard","CB","visa"],
+  ageMinimum : 18,
+
+  verificationAge : function(age){
+    if (age > this.ageMinimum){
+      alert("bienvenu "+baptiste.prenom);
+    }
+    else{
+      alert("vous ne pouvez pas entrer");
+    }
+  },
+  verificationPays : function(paysResidence){
+    for (var i = 0; i < this.paysAutorise.length; i++) {
+      if (this.paysAutorise[i] == paysResidence) {
+        alert("vous êtes dans le bon pays");
+        return "";
+      }
+      else{
+        alert("perdu");
+      }
+    }
+  },
+  verificationPayement : function(mPayement){
+    console.log(baptiste.mPayement);
+    console.log(this.payementAutorise);
+    for (var i = 0; i < this.payementAutorise.length; i++) {
+      for (var j = 0; j < baptiste.mPayement.length; j++) {
+        if (this.payementAutorise[i]==mPayement[j]) {
+          alert("vous pouvez acheter");
+          return "";
+        }
+        else{
+          var erreur= 0;
+          erreur++;
+          if(erreur == this.payementAutorise.length){
+            alert("vous pouvez pas acheter");
+          }
+        }
+      }
+    }
+  },
+  lancement : function(){
+    verificationUser.verificationAge(baptiste.age);
+    verificationUser.verificationPays(baptiste.paysResidence);
+    verificationUser.verificationPayement(baptiste.mPayement);
+  },
+};
+verificationUser.verificationPayement(baptiste.mPayement);
+/*
+verificationUser.verificationAge(baptiste.age);
+verificationUser.verificationPays(baptiste.paysResidence);
+
+verificationUser.lancement();*/
+/*
+
+
+
+
+/******************************************/
+/******************************************/
+/******************************************/
+/**********************Exo 2***************/
+/******************************************/
+/******************************************/
+/******************************************/
+/******************************************/
+
+
+
+/*
+function guerrier(nom,attaque,defense,vie){
+  this.nom = nom,
+  this.attaque = attaque,
+  this.defense = defense,
+  this.vie = vie,
+
+  this.attaqueGuerrier = function(ennemy){
+    while(this.vie > 0 && ennemy.vie > 0){
+      this.vie -= ennemy.attaque - this.defense;
+      ennemy.vie -= this.attaque - ennemy.defense;
+      console.log(this.nom+" "+this.vie +" "+ennemy.nom+" "+ ennemy.vie);
+    }
+    if (this.vie > 0) {
+      alert(this.nom + " gagne!");
+    }
+    else {
+      alert(ennemy.nom+ " gagne!");
+    }
+  }
+}
+
+function magicien(nom, attaque, defense, vie, mana){
+  this.nom = nom,
+  this.attaque = attaque,
+  this.defense = defense,
+  this.vie = vie,
+  this.mana = mana,
+
+  this.soin = function(){
+    if(mana > 10){
+      this.vie += 10;
+      this.mana -= 10;
+    }
+    else {
+      console.log("plus assez de mana");
+    }
+  }
+  this.bouleDeFeu = function(guerrier1, guerrier2){
+    guerrier1.vie -= this.attaque;
+    guerrier2.vie -= this.attaque;
+  }
+
+  this.attaqueMage = function(guerrier1, guerrier2){
+
+    while (this.vie > 0 && guerrier1.vie > 0 || guerrier2.vie > 0) {
+
+      this.vie -= guerrier1.attaque - this.defense;
+      this.vie -= guerrier2.attaque - this.defense;
+      this.soin();
+      this.bouleDeFeu(guerrier1, guerrier2);
+      console.log(this.vie +" "+guerrier1.vie+" "+guerrier2.vie);
+    }
+
+    if (this.vie > 0) {
+      alert(this.nom+ " gagne!");
+    }
+    else{
+      alert(guerrier1.nom +" et "+guerrier2+" gagnent!");
+    }
+  }
+}
+
+var illidan = new guerrier("illidan","23","15","50");
+var arthas = new guerrier("arthas","18","20","60");
+var kadgar = new magicien("kadgar", "20", "10","40","60");
+//arthas.attaqueGuerrier(illidan);
+kadgar.attaqueMage(arthas, illidan);
+*/
+
+
+
+/******************************************/
+/******************************************/
+/******************************************/
+/**********************Exo 3***************/
+/******************************************/
+/******************************************/
+/******************************************/
+/******************************************/
+
+
+/*
+
+function recupDonnee(){
+      var donnee = [];
+      var recupTitre = $("#titre").val();
+      donnee.push(recupTitre);
+      var recupAccroche = $("#accroche").val();
+      donnee.push(recupAccroche);
+      var recupDescription = $("#description").val();
+      donnee.push(recupDescription);
+      console.log(donnee);
+      var nouvelObjet = new ficheProduit(donnee[0],donnee[1],donnee[2]);
+      nouvelObjet.creationFiche(titre, accroche, description);
+}
+
+function ficheProduit(titre, accroche, description){
+  this.titre = titre,
+  this.accroche = accroche,
+  this.description = description,
+
+  this.creationFiche = function(){
+    console.log(this.titre);
+    $("#resultat").append("<article class='"+this.titre+"  ficheProduit'></article>");
+    $("."+this.titre).append("<h2>"+this.titre+"</h2>");
+    $("."+this.titre).append("<h3>"+this.accroche+"</h3>");
+    $("."+this.titre).append("<p>"+this.description+"</p>");
+    $("#titre").val("");
+    $("#accroche").val("");
+    $("#description").val("");
+    }
+}
+*/
+
+/********************************************************************/
+/********************************************************************/
+/********************************************************************/
+/********************************************************************/
+/********************************************************************/
+/*****************EXERCICE 4*****************************************/
+/********************************************************************/
+/********************************************************************/
+/********************************************************************/
+/********************************************************************/
+
+// ancien code de compatibilité, aujourd’hui inutile
+/*function makeRequest(url) {
+
+        var httpRequest = false;
+
+        httpRequest = new XMLHttpRequest();
+
+        if (!httpRequest) {
+            alert('Abandon :( Impossible de créer une instance XMLHTTP');
+            return false;
+        }
+        httpRequest.onreadystatechange = function() { alertContents(httpRequest); };
+        httpRequest.open('GET', "exo.json", true);
+        httpRequest.send(null);
+
+    }
+
+    function alertContents(httpRequest) {
+
+        if (httpRequest.readyState == XMLHttpRequest.DONE) {
+            if (httpRequest.status == 200) {
+                alert(httpRequest.responseText);
+            } else {
+                alert('Un problème est survenu avec la requête.');
+            }
+        }
+
+    }
+*/
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xmlhttp.open("GET", "exo.json", true);
+  xmlhttp.send();
